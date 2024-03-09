5504d61edae416586868a4dccf3a7272849ac815
Kirill Medvedev <repkomail@gmail.com>
2024-03-09 16:59:45 +0200

    feat: implement first map data and add types for project

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,31 @@
+import { useEffect, useState } from 'react'
 import './App.css'
+import { Group } from './types/GroupsTypes';
 
 function App() {
 
+  const [data, setData] = useState<Group[]>([]);
+
+  useEffect(() => {
+    const fetchGroups = async () => {
+      const response = await fetch('./data/groups.json')
+      const data = await response.json();
+      console.log(data);
+      setData(data)
+    }
+    fetchGroups()
+  }, [])
+
   return (
     <>
-      <div>Hello world</div>
+    <h1 className='app__title'>Group filter</h1>
+    <div>
+      {data.map((el) => (
+        <div key={el.id}>
+          <p>{el.name}</p>
+        </div>
+      ))}
+    </div>
     </>
   )
 }
